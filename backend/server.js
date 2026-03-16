046680f15bb19b6be98dd9051b98c52fa6e5ff76
saarah-m <halloweentown1019@gmail.com>
2026-03-16 10:16:33 -0700

    Fixed server error

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Handle any other route by returning the React app
-app.get("*", (req, res) => {
+app.get("/{*path}", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
